5f604f1efa780f7a5abf015e89ddaf6cb599a184
Swetha Suresh <112751881+swethasures@users.noreply.github.com>
2022-10-10 18:19:24 +0530

    Add files via upload

---
+++ D 4 product single inheritance.cpp
@@ -0,0 +1,34 @@
+#include <iostream> 
+using namespace std; 
+class base    
+{
+   public:
+     int x;
+   void getdata()
+   {
+     cout << "Enter the value of x = "; cin >> x;
+   }
+ };
+class derive : public base    
+{
+   private:
+    int y;
+   public:
+   void readdata()
+   {
+     cout << "Enter the value of y = "; cin >> y;
+   }
+   void product()
+   {
+     cout << "Product = " << x * y;
+   }
+ };
+ 
+ int main()
+ {
+    derive a;     
+    a.getdata();
+    a.readdata();
+    a.product();
+    return 0;
+ }  
